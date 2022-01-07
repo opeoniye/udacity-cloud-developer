4e1840bb96fd9f272fc6c13a37630d72ee21568d
Samuel John <opeoniye@gmai.com>
2022-01-07 20:56:32 +0100

    monolith to microservices

---
+++ project3/env.sh
@@ -0,0 +1,10 @@
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=bamsudacity
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=udagram
+export AWS_BUCKET=bamsudacity-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=udacity
+export JWT_SECRET=udagram4bams
+export URL=http://localhost:8100
